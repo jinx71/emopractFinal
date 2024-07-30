09d2d8b4485b0bbd08cabbe23134c357feb9e923
jinx71 <karim.sazed@gmail.com>
2024-07-31 03:28:33 +0600

    home page slider autoplay true

--- src/ClientSide/Home/HeroSection/HeroSection.jsx
+++ src/ClientSide/Home/HeroSection/HeroSection.jsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
   };
